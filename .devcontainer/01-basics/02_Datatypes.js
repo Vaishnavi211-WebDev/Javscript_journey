c6f4b84912b778036ffb2fc2ef549dd260ffa432
Vaishnavi211-WebDev <athawalevaishnavi66@gmail.com>
2025-10-05 08:23:34 +0000

    Datatypes

---
+++ .devcontainer/01-basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using node js
+
+let name ="vaishnavi" //string
+let age = 18
+let isLoggedIn = false //boolean
+let state = null;
+
+//number =? 2 to power 53
+//bignit
+//string=> ""
+//boolean true/false
+//null standalone value no value assign yet
+//undefined=> no value assigned 
+//symbol => unique
+
+//object
+//undefined is type
+//null is an object
+
+console.log(typeof age);
---
+++ .devcontainer/01-basics/Variables.js
@@ -0,0 +1,11 @@
+const accountId = 2110010
+let accountEmail = "vaishnaviathawale@gmail.com"
+var accountPassword = "12345"
+// accountCity = "jaipur" //bad practice it on;y reserve space
+
+// let accountState; //will give output undefined
+
+console.table([accountId,accountEmail,accountPassword,accountState])
+
+/*prefer not to use var
+(beacuse it is not block or function scope)*/
