9ad47481bda6d5dfa5077ce7a44c9b8f53784e45
Vaishnavi211-WebDev <athawalevaishnavi66@gmail.com>
2025-10-05 15:44:50 +0000

    operations done

--- .devcontainer/03_conversionOperation.js
+++ .devcontainer/03_conversionOperation.js
@@ -1,38 +1,43 @@
-let score = 
-// // //here we know that score has number but some times we dont know 
-// // the type so we will check using typeof
-// // there are two ways
+// let score = 
+// // // //here we know that score has number but some times we dont know 
+// // // the type so we will check using typeof
+// // // there are two ways
 
- console.log(typeof score);
- console.log(typeof(score));
+//  console.log(typeof score);
+//  console.log(typeof(score));
 
-// // //so if we want to perform operation on string strictly then 
-// // we can do
+// // // //so if we want to perform operation on string strictly then 
+// // // we can do
 
-// let valueInNumber = Number(score)
+// // let valueInNumber = Number(score)
 
- console.log(typeof valueInNumber); //strictly convert into number
- console.log(valueInNumber);
+//  console.log(typeof valueInNumber); //strictly convert into number
+//  console.log(valueInNumber);
 
-// //33abc is not a complete number but js it will convert but actully value
-// // output will NAN
+// // //33abc is not a complete number but js it will convert but actully value
+// // // output will NAN
 
-// // let score = null
-// // if null it will give 0
+// // // let score = null
+// // // if null it will give 0
 
-// //let score = undefined
-// //output = NAN
+// // //let score = undefined
+// // //output = NAN
 
-// //let score = true/false
-// //output = 0/1
+// // //let score = true/false
+// // //output = 0/1
 
-// //"33" => 33
-// //"33abc" =>NAN
+// // //"33" => 33
+// // //"33abc" =>NAN
 
-let isLoggedIn =""
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn =""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-//1 => true; 0=>false;
-//" => false
-//"hitesh" =>true
+// //1 => true; 0=>false;
+// //" => false
+// //"hitesh" =>true
+
+
+//*************************Operations**************************
+
+  
